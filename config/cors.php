c4fed856c1cb8b60104bc05bfcc12c3d5f23bb48
Maryam Mohamed Auf <maryammohamedauf@gmail.com>
2024-08-25 08:48:40 +0300

    Prepare for CI/CD

--- config/cors.php
+++ config/cors.php
@@ -3,9 +3,10 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'],
+    //'allowed_origins' => ['http://localhost:3000'],
   // 'allowed_origins' => ['http://192.168.1.5:3000'],
-   
+  'allowed_origins' => ['*'],
+
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
