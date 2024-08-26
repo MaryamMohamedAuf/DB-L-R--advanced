4a873491034929aa796e1f9dfa043e97fc52a512
Maryam Mohamed Auf <maryammohamedauf@gmail.com>
2024-08-26 08:08:55 +0300

    Hosting the database

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,7 @@ use App\Http\Controllers\SurveyController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/cohorts/index', [CohortController::class, 'index'])->name('cohorts.index');
-Route::post('/register', [RegisteredUserController::class, 'store'])->middleware('guest')->name('register');
+Route::post('/register', [RegisteredUserController::class, 'store']);
 
 Route::post('/login', [AuthenticatedSessionController::class, 'store']);
 
